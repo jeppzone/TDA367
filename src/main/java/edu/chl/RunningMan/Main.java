ee3481faa23e3db21aa700586a10af633bce2ce5
jeppzone <jespols@student.chalmers.se>
2015-03-24 11:12:35 +0100

    Removed some packages and renamed everything

--- src/main/java/edu/chl/RunningMan/Main.java
+++ src/main/java/edu/chl/RunningMan/Main.java
@@ -1,9 +1,4 @@
-package edu.chl.change2projectname;
-
-import edu.chl.change2projectname.controller.ProjectController;
-import edu.chl.change2projectname.model.Project;
-import edu.chl.change2projectname.view.ProjectView;
-import javax.swing.SwingUtilities;
+package edu.chl.RunningMan;
 
 /*
   Application entry class (if using standard java and Swing)
@@ -14,12 +9,6 @@ public final class Main {
 	}
 
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-                    final Project project = new Project();
-                    final ProjectView projectView = new ProjectView(project);
-                    
-                    ProjectController.create(project, projectView);
-                    projectView.setVisible(true);
-                });
-	}
+        System.out.println("Hello World");
+    }
 }
