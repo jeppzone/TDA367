1b38e5096678a2aa036352f0a7b7d632eccfec5a
jeppzone <jespols@student.chalmers.se>
2015-03-30 16:37:12 +0200

    Might have setup libgdx project right

---
+++ RunningMan/desktop/src/edu/chalmers/RunningMan/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.chalmers.RunningMan.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.chalmers.RunningMan.RunningMan;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RunningMan(), config);
+	}
+}
