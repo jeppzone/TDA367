acc97d05629117b9887fd378935642615ec28cd9
Simon Lindkvist <simon_lindkvist92@hotmail.com>
2015-04-20 10:24:44 +0200

    class Player shell

---
+++ RunningMan/core/src/edu/chalmers/RunningMan/entities/Player.java
@@ -0,0 +1,19 @@
+package edu.chalmers.RunningMan.entities;
+
+/**
+ * Created by Kvist1 on 2015-04-20.
+ */
+public class Player {
+
+    private int hp;
+    private Weapon weapon;
+
+    public Player(int hp, Weapon weapon) {
+        this.hp = hp;
+        this.weapon = Weapon;
+    }
+
+    public void move() {}
+    public void jump() {}
+    public void shoot() {}
+}
