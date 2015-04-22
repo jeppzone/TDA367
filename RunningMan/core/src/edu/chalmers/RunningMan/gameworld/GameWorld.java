e45f48b73527c747c7105c7a1373bef7940a1ff3
JohanTobin <johantobin@gmail.com>
2015-04-22 17:18:40 +0200

    Added GameRenderer, GameWorld

---
+++ RunningMan/core/src/edu/chalmers/RunningMan/gameworld/GameWorld.java
@@ -0,0 +1,14 @@
+package edu.chalmers.RunningMan.gameworld;
+
+import com.badlogic.gdx.Gdx;
+
+/**
+ * Created by JohanTobin on 2015-04-22.
+ */
+public class GameWorld {
+    public void update(float delta) {
+
+        Gdx.app.log("GameWorld", "update");
+
+    }
+}
