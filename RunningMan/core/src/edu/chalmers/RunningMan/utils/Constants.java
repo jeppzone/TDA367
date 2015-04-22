c2b10e7bbfe3bc47dabfb0e67882bf8b5d1dc198
JohanTobin <johantobin@gmail.com>
2015-04-22 17:13:17 +0200

    Added Class contants

---
+++ RunningMan/core/src/edu/chalmers/RunningMan/utils/Constants.java
@@ -0,0 +1,11 @@
+package edu.chalmers.RunningMan.utils;
+
+/**
+ * Created by JohanTobin on 2015-04-22.
+ */
+public class Constants {
+
+    public static final int V_WIDTH = 320;
+    public static final int V_HEIGHT = 240;
+
+}
