155ea5e6ba65ce9fd6e28b66ca536e5de9c72f96
Armand Ghaffarpour <armand.ghaffarpour@gmail.com>
2015-04-22 16:11:27 +0200

    Made an abstract View for livingobjects

---
+++ RunningMan/core/src/edu/chalmers/RunningMan/views/AbstractLivingObjectView.java
@@ -0,0 +1,29 @@
+package edu.chalmers.RunningMan.views;
+
+import edu.chalmers.RunningMan.entities.AbstractLivingObject;
+
+import java.beans.*;
+
+/**
+ * Created by Armand on 2015-04-22.
+ */
+public abstract class AbstractLivingObjectView implements PropertyChangeListener {
+
+    private AbstractLivingObject alo;
+
+    public AbstractLivingObject getAlo(){
+        return alo;
+    }
+
+    public void setAlo(AbstractLivingObject a){
+        if(a != null)
+            a.removePropertyChangeListener(this);
+            alo = a;
+        if(alo != null) {
+            alo.addPropertyChangeListener(this);
+        }
+    }
+
+    public abstract void propertyChange(PropertyChangeEvent e);
+    
+}
--- RunningMan/core/src/edu/chalmers/RunningMan/views/PlayerView.java
+++ RunningMan/core/src/edu/chalmers/RunningMan/views/PlayerView.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import edu.chalmers.RunningMan.entities.Player;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 /**
  * Created by JohanTobin on 2015-04-20.
  */
-public class PlayerView extends Actor {
+public class PlayerView extends Actor implements PropertyChangeListener {
 
     public PlayerView(Player pm ){
 
@@ -16,4 +19,10 @@ public class PlayerView extends Actor {
 
     }
 
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+
+
+    }
 }
