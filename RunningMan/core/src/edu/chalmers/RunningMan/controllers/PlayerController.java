aabc36ed74f72eb9f709739c900170ea6c5bed8d
Simon Lindkvist <simon_lindkvist92@hotmail.com>
2015-04-23 12:28:24 +0200

    Fixed PlayerController and GameWorld so input works

--- RunningMan/core/src/edu/chalmers/RunningMan/controllers/PlayerController.java
+++ RunningMan/core/src/edu/chalmers/RunningMan/controllers/PlayerController.java
@@ -1,26 +1,47 @@
 package edu.chalmers.RunningMan.controllers;
 
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import edu.chalmers.RunningMan.entities.Player;
+import edu.chalmers.RunningMan.handlers.Input;
+import edu.chalmers.RunningMan.views.PlayerView;
 
 /**
  * Created by JohanTobin on 2015-04-20.
  */
 public class PlayerController  {
+
     private Player player;
-    public PlayerController(Player player){
+    private PlayerView playerView;
+
+    public PlayerController(Player player, PlayerView playerView){
         this.player = player;
-    }
-    public void handleKeyboardInput(Input input, int deltaTime){
-        if(input.isKeyPressed(Input.Keys.LEFT)){
-            player.moveLeft(deltaTime);
-        }else if(input.isKeyPressed(Input.Keys.RIGHT)){
-            player.moveRight(deltaTime);
-        }else if(input.isKeyPressed(Input.Keys.SPACE)){
-            player.jump(deltaTime);
-        }
-        player.applyForce(deltaTime);
+        this.playerView = playerView;
     }
 
+    public void handleKeyboardInput(float deltaTime){
+
+
+        // player jump
+        if(Input.isPressed(Input.JUMPBTN)) {
+            System.out.println("jump");
+            player.jump(deltaTime);
+        } else if(Input.isDown(Input.RIGHTBTN)) {
+            System.out.println("go right");
+            player.moveRight(deltaTime);
+        } else if(Input.isDown(Input.LEFTBTN)) {
+            System.out.println("go left");
+            player.moveLeft(deltaTime);
+        } else if(Input.isPressed(Input.SHOOTBTN)) {
+            // player.shoot(deltaTime);
+        }
+        player.applyForce(deltaTime);
+
+    }
+
+    public void update(float deltaTime) {
+
+        handleKeyboardInput(deltaTime);
+
+        // playerView.render();
+    }
 }
--- RunningMan/core/src/edu/chalmers/RunningMan/gameworld/GameWorld.java
+++ RunningMan/core/src/edu/chalmers/RunningMan/gameworld/GameWorld.java
@@ -1,14 +1,45 @@
 package edu.chalmers.RunningMan.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import edu.chalmers.RunningMan.controllers.PlayerController;
+import edu.chalmers.RunningMan.entities.Player;
+import edu.chalmers.RunningMan.entities.Position;
+import edu.chalmers.RunningMan.entities.Size;
+import edu.chalmers.RunningMan.entities.Weapon;
+import edu.chalmers.RunningMan.views.PlayerView;
+
+import static edu.chalmers.RunningMan.utils.Constants.V_HEIGHT;
+import static edu.chalmers.RunningMan.utils.Constants.V_WIDTH;
 
 /**
  * Created by JohanTobin on 2015-04-22.
  */
 public class GameWorld {
-    public void update(float delta) {
 
-        Gdx.app.log("GameWorld", "update");
+    private OrthographicCamera cam;
+
+    private PlayerController playerController;
+    private Player player;
+    private PlayerView playerView;
+
+    public GameWorld() {
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, V_WIDTH, V_HEIGHT);
+
+        player = new Player(new Weapon(), new Position(0,0), new Size(50,50), 100);
+        playerView = new PlayerView(player);
+        playerController = new PlayerController(player, playerView);
+
+    }
+
+    public void update(float deltaTime) {
+
+        //Gdx.app.log("GameWorld", "update");
+
+        playerController.update(deltaTime);
 
     }
 }
