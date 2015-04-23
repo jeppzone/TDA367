0fd75f7ad589d19d2e87a2e25688c3f5eb573b26
Armand Ghaffarpour <armand.ghaffarpour@gmail.com>
2015-04-23 11:14:36 +0200

    Added a testpicture in assets and started developing PlayerView

--- RunningMan/core/src/edu/chalmers/RunningMan/views/PlayerView.java
+++ RunningMan/core/src/edu/chalmers/RunningMan/views/PlayerView.java
@@ -1,6 +1,9 @@
 package edu.chalmers.RunningMan.views;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
@@ -8,17 +11,30 @@ import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import edu.chalmers.RunningMan.entities.Player;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 
-/**
- * Created by JohanTobin on 2015-04-20.
- */
+
 public class PlayerView{
 
-    public PlayerView(Player pm){
+    private final Player player;
+    private final Sprite sprite;
+    //private final TextureRegion standingLeft;
+    //private final TextureRegion standingRight;
+    private final Texture stand;
+
+
+
+    public PlayerView(Player player, Sprite sprite){
+        this.player = player;
+        this.sprite = sprite;
         Image img = new Image();
+        stand = new Texture(Gdx.files.internal("tesPic.png"));
+        sprite = new Sprite();
+    }
+
+    public void draw(){
+
     }
 
 }
