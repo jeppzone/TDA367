854330e6f0cc8c0106d7374eeb2dd695a083a1e4
JohanTobin <johantobin@gmail.com>
2015-05-07 08:10:44 +0200

    Added audiohandler and audiocontroller

---
+++ RunningMan/core/src/main/edu/chalmers/RunningMan/controllers/AudioController.java
@@ -0,0 +1,16 @@
+package edu.chalmers.RunningMan.controllers;
+
+import edu.chalmers.RunningMan.handlers.AudioHandler;
+
+/**
+ * Created by JohanTobin on 2015-05-06.
+ */
+public class AudioController {
+
+    public AudioHandler audio;
+
+    public AudioController(){
+        audio = new AudioHandler();
+    }
+
+}
---
+++ RunningMan/core/src/main/edu/chalmers/RunningMan/handlers/AudioHandler.java
@@ -0,0 +1,57 @@
+package edu.chalmers.RunningMan.handlers;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import java.io.IOException;
+
+/**
+ * Created by JohanTobin on 2015-05-06.
+ */
+public class AudioHandler {
+    private final static String AUDIO_LOCATION = "core/assets/audio/";
+
+    private Sound jump;
+    private Sound shoot;
+    private Sound die;
+    private Sound steroid;
+    private Sound startLevel;
+    private Music levelMusic;
+
+    public AudioHandler() {
+        loadAudio();
+    }
+
+    private void loadAudio() {
+        jump = Gdx.audio.newSound(Gdx.files.internal(AUDIO_LOCATION + "jump.wav"));
+        shoot = Gdx.audio.newSound(Gdx.files.internal(AUDIO_LOCATION + "shoot.wav"));
+        die = Gdx.audio.newSound(Gdx.files.internal(AUDIO_LOCATION + "die.wav"));
+        steroid = Gdx.audio.newSound(Gdx.files.internal(AUDIO_LOCATION + "steroid.wav"));
+        startLevel = Gdx.audio.newSound(Gdx.files.internal(AUDIO_LOCATION + "startLevel.mp3"));
+        levelMusic = Gdx.audio.newMusic(Gdx.files.internal(AUDIO_LOCATION + "music.mp3"));
+    }
+
+    public void playJumpSound() {
+        jump.play();
+    }
+
+    public void playShootSound() {
+        shoot.play();
+    }
+
+    public void playDieSound() {
+        die.play();
+    }
+
+    public void  play (){
+        steroid.play();
+    }
+
+    public void playStartLevelSound() {
+        startLevel.play();
+    }
+
+    public void playMusic(){
+        levelMusic.play();
+    }
+}
