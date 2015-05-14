74aeff2782c225fb1fd99cd51c2927dda89cd85e
Armand Ghaffarpour <armand.ghaffarpour@gmail.com>
2015-05-14 10:19:26 +0200

    fixed name of BulletTest

---
+++ RunningMan/core/src/test/edu/chalmers/RunningMan/entities/BulletTest.java
@@ -0,0 +1,25 @@
+package edu.chalmers.RunningMan.entities;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Armand on 2015-05-14.
+ */
+public class BulletTest extends TestCase {
+
+    public void testGetBulletSpeed() throws Exception {
+
+    }
+
+    public void testMoveBullet() throws Exception {
+
+    }
+
+    public void testIsOutOfBounds() throws Exception {
+
+    }
+
+    public void testAcceptVisitor() throws Exception {
+
+    }
+}
