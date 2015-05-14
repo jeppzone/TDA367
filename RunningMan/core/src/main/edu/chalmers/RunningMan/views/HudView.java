0b4ecc08a73acb25985973acf69a34a504f72673
jeppzone <jespols@student.chalmers.se>
2015-05-14 14:24:03 +0200

    added HudView

---
+++ RunningMan/core/src/main/edu/chalmers/RunningMan/views/HudView.java
@@ -0,0 +1,16 @@
+package edu.chalmers.RunningMan.views;
+
+import com.badlogic.gdx.graphics.g2d.freetype.FreeType;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import edu.chalmers.RunningMan.entities.Level;
+
+/**
+ * Created by Jesper on 5/14/2015.
+ */
+public class HudView extends Stage {
+
+    private TimeView imeView;
+
+    public HudView(Level level){
+    }
+}
