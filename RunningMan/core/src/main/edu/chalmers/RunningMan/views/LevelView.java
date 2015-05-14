a94ffced0f60918a7faf4d331662414044894fbe
jeppzone <jespols@student.chalmers.se>
2015-05-14 14:30:31 +0200

    New constructor in LevelView

--- RunningMan/core/src/main/edu/chalmers/RunningMan/views/LevelView.java
+++ RunningMan/core/src/main/edu/chalmers/RunningMan/views/LevelView.java
@@ -3,6 +3,7 @@ package edu.chalmers.RunningMan.views;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import edu.chalmers.RunningMan.entities.Level;
@@ -18,14 +19,12 @@ public class LevelView extends Stage{
     private List<Actor> views;
     private Batch batch;
     private Player player;
-    private TimeView timeView;
     private OrthographicCamera camera;
     private PlayerView playerView;
 
-    public LevelView(Level level, List<Actor> views, Player player, BulletView bulletView ){
+    public LevelView(List<Actor> views, Player player, BulletView bulletView ){
         this.player = player;
         this.views = views;
-        timeView =  new TimeView(level.getTime());
         views.add(bulletView);
         playerView = new PlayerView(player);
         initCamera();
@@ -49,7 +48,6 @@ public class LevelView extends Stage{
         float deltaTime = Gdx.graphics.getDeltaTime();
         batch = getBatch();
         playerView.draw(batch, deltaTime);
-        timeView.draw(batch, deltaTime);
         updateCamera();
         for(Actor actor: views){
             actor.draw(batch, deltaTime);
