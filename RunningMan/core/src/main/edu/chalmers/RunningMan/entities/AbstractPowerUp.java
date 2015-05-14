1ffe3690118e16d3eb8d71a5a8178db5aadd5b17
jeppzone <jespols@student.chalmers.se>
2015-05-14 09:29:35 +0200

    Fixed Time for steroids, changed name of AbstractCollectible to Abstract Power Up

--- RunningMan/core/src/main/edu/chalmers/RunningMan/entities/AbstractPowerUp.java
+++ RunningMan/core/src/main/edu/chalmers/RunningMan/entities/AbstractPowerUp.java
@@ -4,17 +4,28 @@ package edu.chalmers.RunningMan.entities;
  * Abstract class for collectible objects
  * @author Jesper Olsson
  */
-public abstract class AbstractCollectibleObject extends AbstractPhysicalObject {
+public abstract class AbstractPowerUp extends AbstractPhysicalObject implements ICollectible {
     protected boolean pickedUp;
-    public AbstractCollectibleObject(Size size, Position position){
+    protected Time time;
+    public AbstractPowerUp(Size size, Position position){
         super(size ,position);
         setPickedUp(false);
+        time = new Time();
     }
     public void setPickedUp(boolean pickedUp){
         this.pickedUp = pickedUp;
     }
     public boolean isPickedUp(){
         return pickedUp;
+    }
+    public void updateTime(float deltaTime){
+        if(isPickedUp()){
+            time.update(deltaTime);
+        }
+    }
+
+    public Time getTime(){
+        return time;
     }
     abstract public void acceptVisitor(IVisitor visitor);
 
