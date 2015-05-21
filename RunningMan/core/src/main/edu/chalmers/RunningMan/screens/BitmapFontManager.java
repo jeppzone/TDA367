c9d8079f06f3c5ac18484382897b56ab97bd4db0
Simon Lindkvist <simon_lindkvist92@hotmail.com>
2015-05-21 11:06:53 +0200

    Added classes to make it easier to create our menu buttons

---
+++ RunningMan/core/src/main/edu/chalmers/RunningMan/screens/BitmapFontManager.java
@@ -0,0 +1,44 @@
+package edu.chalmers.RunningMan.screens;
+
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+/**
+ * A class which can create a group of bitmap fonts
+ */
+public class BitmapFontManager {
+
+    private static BitmapFont menuButtonFontBlack, menuButtonFontWhite;
+    private int fontSize = 70;
+
+    public BitmapFontManager() {
+        createFonts();
+    }
+
+    private void createFonts() {
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("core/assets/fonts/StarFont.TTF"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = fontSize;
+        parameter.color = Color.BLACK;
+        menuButtonFontBlack = generator.generateFont(parameter);
+
+        parameter.color = Color.WHITE;
+        menuButtonFontWhite = generator.generateFont(parameter);
+
+        // if more fonts, create them here
+
+        generator.dispose();
+    }
+
+    public BitmapFont createNewMenuButtonFontBlack() {
+        return menuButtonFontBlack;
+    }
+
+    public BitmapFont createNewMenuButtonFontWhite() {
+        return menuButtonFontWhite;
+    }
+}
